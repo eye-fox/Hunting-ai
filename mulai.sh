c376889c0d3d6521300455b7f1697971f5f10e81
EyeFox <pentesting833@gmail.com>
2026-08-06 05:42:53 +0800

    Update mulai.sh

--- mulai.sh
+++ mulai.sh
@@ -11,4 +11,4 @@ mkdir -p ~/.local/share/opencode
 PROMPT=$(cat prompt.txt)
 
 # Jalankan OpenCode dengan isi prompt.txt
-~/.opencode/bin/opencode run "$PROMPT" -m opencode/laguna-s-2.1-free --auto
+~/.opencode/bin/opencode run "$PROMPT" -agent 'Flow Hunter' -m opencode/laguna-s-2.1-free --auto
